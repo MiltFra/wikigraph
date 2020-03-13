4a03dca8eeeb6754878b9921155d555945c1bb93
Franz Miltz <public@miltz.me>
2020-03-13 19:07:16 +0000

    Made config more idiomatic using iterators.

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,15 @@
-use thiserror::Error;
+use super::url::URL;
 use std::error::Error;
 use std::fs;
-use super::url::URL;
+use thiserror::Error;
 /// ConfigErr is an enum that contains possible error values that
 /// could occur during the Configuration of this library in Config::new.
 #[derive(Error, Debug)]
 pub enum ConfigErr {
-    #[error("Found too many arguments. (expected: 2, found: {0})")]
-    TooManyArguments(usize),
-    #[error("Found too few arguments. (expected: 2, found: {0})")]
-    TooFewArguments(usize),
+    //#[error("Found too many arguments. (expected: 2)")]
+    //TooManyArguments,
+    #[error("Found too few arguments. (expected: 2)")]
+    TooFewArguments,
     #[error("Could not parse search depth. (found {0})")]
     IntParseError(String),
     #[error("Found no valid urls in the file.")]
@@ -31,27 +31,25 @@ impl Config {
     /// Excatly two arguments are expected, otherwise an error is returned.
     /// - An integer containing the desired search depth.
     /// - A file name containing the starting URLs.
-    pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
-        let len = args.len();
-        // Checking args for len==2
-        if len < 3 {
-            return Err(Box::new(ConfigErr::TooFewArguments(len)));
-        }
-        if len > 3 {
-            return Err(Box::new(ConfigErr::TooManyArguments(len)));
-        }
-        // Parsing the depth n
-        let r = args[0].parse();
-        let n;
-        match r {
-            Err(_) => return Err(Box::new(ConfigErr::IntParseError(args[0].clone()))),
-            Ok(v) => n = v,
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, Box<dyn Error>> {
+        // Dropping the name of the executable.
+        args.next();
+        // Parsing the depth.
+        let n = match args.next() {
+            Some(arg) => match arg.parse() {
+                Ok(v) => v,
+                Err(_) => return Err(Box::new(ConfigErr::IntParseError(arg))),
+            },
+            None => return Err(Box::new(ConfigErr::TooFewArguments)),
+        };
         // Parsing the URL file
-        let r = Config::get_urls(&args[2]);
-        match r {
-            Err(e) => return Err(e),
-            Ok(v) => return Ok(Config { urls: v, depth: n }),
+        let urls = match args.next() {
+            Some(arg) => Config::get_urls(&arg),
+            None => return Err(Box::new(ConfigErr::TooFewArguments)),
+        };
+        match urls {
+            Err(e) => Err(e),
+            Ok(v) => Ok(Config { urls: v, depth: n }),
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::process;
 use wglib::config::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let cfg = Config::new(&args).unwrap_or_else(|err| {
+    let cfg = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
