78d141bd0a6c747bdc23934fc509d2bf62b0adff
Franz Miltz <public@miltz.me>
2020-03-14 15:25:59 +0000

    Implemented Article placeholder

--- src/article.rs
+++ src/article.rs
@@ -1,11 +1,15 @@
 use super::*;
-use reqwest;
 use std::error::Error;
 use thiserror::Error;
 
 /// A struct representing a Wikipedia article with attributes like
-/// the URL, the Head Line, related articles, etc.
-pub struct Article {}
+/// the URL, related articles and eventually more.
+pub struct Article {
+    /// URL of the article; where you'd find it in your web browser.
+    url: URL,
+    /// All the URLs of other articles that are referenced within the article.
+    references: Vec<URL>,
+}
 
 /// ArticleErr is an enum that contains possible error values that
 /// could occur during the creation of a new Article in Article::new.
@@ -14,17 +18,10 @@ pub struct Article {}
 pub enum ArticleErr {}
 
 impl Article {
-    pub async fn get(client: reqwest::Client, url: url::URL) -> Result<Article, Box<dyn Error>> {
-        let body = client.get(&url.to_string()).send().await?;
-        // TODO: Read about concurrency and implement proper ascynchronous requests.
-        Ok(Article {})
-    }
-
-    // TODO: Use closures and iterators to make this more functional and idiomatic.
-    pub fn get_list(
-        client: reqwest::Client,
-        urls: Vec<url::URL>,
-    ) -> Result<Vec<Article>, Box<dyn Error>> {
-        Ok(Vec::new())
+    pub fn new(url: URL) -> Self {
+        Article {
+            url: url,
+            references: Vec::new(),
+        }
     }
 }
